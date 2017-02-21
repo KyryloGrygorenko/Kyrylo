58d906cf3aa042c16409c43646ecb65abffe8874
KyryloGrygorenko <kyrylogrygorenko@gmail.com>
2017-02-21 14:18:19 +0000

    HomeTask PHP_basics corrections #8,#15

--- php_basics_tasks/1-8.php
+++ php_basics_tasks/1-8.php
@@ -1,22 +1,24 @@
+
 <?php
 
 $name= 'Кирилл';
-$age= 'test';
+$age= 22;
 
 echo "Меня зовут: {$name} </br>";
 
-echo "Мне {$age} лет </br>";
 
-if ($age >=18 & $age <=59) {
-    echo "Вам еще работать и работать";
+if (is_numeric($age)) {
+    echo "Мне {$age} лет </br>";
+    if ($age >=18 & $age <=59) {
+        echo "Вам еще работать и работать";
 
-} elseif ($age >59) {
-    echo "Вам пора на пенсию";
-} elseif ($age >=1 & $age<18)  {
-    echo "Вам еще рано работать";
+    } elseif ($age >59) {
+        echo "Вам пора на пенсию";
+    } elseif ($age >=1 & $age<18)  {
+        echo "Вам еще рано работать";
+    }
 }
+
 else {
     echo "Неизвестный возраст";
 }
-
-
